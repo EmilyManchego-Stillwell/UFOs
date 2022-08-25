10b00a548d0f2514e03f92ece3580a8bfda7eff9
Emily Manchego-Stillwell <emanchego85@gmail.com>
2022-08-25 12:22:20 -0600

    Updated the app.js file to create the table data and filter it when a button is clicked on the web page.

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,9 @@
 // import the data from data.js
 const tableData = data;
+
 // Reference the HTML table using d3
-var tbody = d3.select("tbody")
+var tbody = d3.select("tbody");
+
 function buildTable(data) {
     //First, clear out any existing data
     tbody.html("");
@@ -20,4 +22,25 @@ function buildTable(data) {
             }
         );
     });
-}
+};
+
+function handleClick() {
+    //Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData= tableData;
+    //Check to see if a date was entered and filter the data using that date
+    if (date) {
+        //Apply 'filter' to the table data to only keep the rows where the 
+        //`datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    //Rebuild the table using the filtered data 
+    //@NOTE: If no date was entered, then filteredData will just be the original
+    //tableData
+    buildTable(filteredData);
+};
+
+//Code to 'listen' for a click
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+buildTable(tableData);
